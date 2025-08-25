2c9a88bfb2419b86a76c389e1b98e0ea12a16577
Simone Guarino <120113995+SimoneGuarino@users.noreply.github.com>
2025-08-25 15:02:47 +0200

    nuova sidebar e topbar

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,4 +1,3 @@
-import { useSidebarStore } from "@/hooks/controller";
 import React, { ReactNode } from "react";
 
 type DashboardLayoutProps = {
@@ -6,13 +5,8 @@ type DashboardLayoutProps = {
 };
 
 const DashboardLayout: React.FC<DashboardLayoutProps> = ({ children }) => {
-    const { isOpen } = useSidebarStore();
-
-    // Tailwind classes for base and xl screens
-    // margin-left: 105px if miniSidenav, else 274px (px-0 for base, custom for xl)
-    // transition for margin-left/right
     const baseClasses =
-        "p-6 relative flex flex-col transition-[margin-left,margin-right] duration-300 ease-in-out mt-10";
+        "p-6 relative flex flex-col transition-[margin-left,margin-right] duration-300 ease-in-out mt-12";
 
     return (
         <div className={`${baseClasses}`}>
